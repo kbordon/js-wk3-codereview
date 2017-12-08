09a06ceb80a9999faccec10ac64ddef516381460
Kimberly Bordon <kbordon@gmail.comg>
2017-12-08 09:23:18 -0800

    add animal list component

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 
-@Component({})
+@Component({
+  selector: 'animal-list',
+  template: `
+    <div *ngFor="let currentAnimal of childAnimalList">
+      <h2>{{currentAnimal.name}}</h2>
+      <h3>{{currentAnimal.species}}</h3>
+      <p>{{currentAnimal.age}}</p>
+      <p>{{currentAnimal.sex}}</p>
+      <p>{{currentAnimal.diet}}</p>
+      <p>{{currentAnimal.location}}</p>
+      <p>{{currentAnimal.caretakers}}</p>
+      <p>{{currentAnimal.likes}}</p>
+      <p>{{currentAnimal.dislikes}}</p>
+      <p>EDIT</p>
+    </div>
+  `
+})
 
-
-export class AnimalListComponent{}
+export class AnimalListComponent{
+  @Input() childAnimalList: Animal[];
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,7 @@ import { Animal } from './animal.model';
   selector: 'app-root',
   template: `
   <h1>ANIMALS</h1>
-  <div *ngFor="let currentAnimal of masterAnimalList">
-    <h2>{{currentAnimal.name}}</h2>
-    <h3>{{currentAnimal.species}}</h3>
-    <p>{{currentAnimal.age}}</p>
-    <p>{{currentAnimal.sex}}</p>
-    <p>{{currentAnimal.diet}}</p>
-    <p>{{currentAnimal.location}}</p>
-    <p>{{currentAnimal.caretakers}}</p>
-    <p>{{currentAnimal.likes}}</p>
-    <p>{{currentAnimal.dislikes}}</p>
-    <p>{{currentAnimal.age}}</p>
+  <animal-list [childAnimalList]="masterAnimalList"></animal-list>
   `
 })
 
@@ -23,6 +13,7 @@ export class AppComponent {
   animal1: Animal = new Animal("Artic Fox", "Moon", 2, "Female", "Carnivore", "Northern Trail", 5, "Cool shade", "Loud noises");
   animal2: Animal = new Animal("Ocelot", "Prince", 4, "Male", "Carnivore", "Tropical Rain Forest Building", 6, "Lying in in sunshine", "Toys that are not rope-based")
   animal3: Animal = new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Female", "Herbivore", "Northern Trail", 2, "Delicate roots and leaves", "Loud Noises");
+
   masterAnimalList: Animal[] = [this.animal1, this.animal2, this.animal3];
   selectedAnimal = null;
 }
