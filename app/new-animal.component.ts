2c194b481ccf70aa5228c54b921ce34007492bec
Kimberly Bordon <kbordon@gmail.com>
2017-12-09 21:06:25 -0800

    change submitForm method so animal entries with missing fields will not be added

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -12,22 +12,22 @@ import { Animal } from './animal.model';
     </div>
     <div class="form-body">
       <div class="body-section">
-        <p><span>Species:</span></p>
+        <p><span>Species</span></p>
         <input type="text" #animalSpecies>
         <br>
-        <p><span>Name:</span></p>
+        <p><span>Name</span></p>
         <input type="text" #animalName>
         <br>
-        <p><span>Age:</span></p>
+        <p><span>Age</span></p>
         <input type="number" #animalAge>
         <br>
-        <p><span>Sex:</span></p>
+        <p><span>Sex</span></p>
         <!-- <input type="text" #animalSex> -->
         <div>
         <input name="sex" type="radio" (change)="animalSex='Female'">Female
         <input name="sex" type="radio" (change)="animalSex='Male'">Male
         </div>
-        <p><span>Diet:</span></p>
+        <p><span>Diet</span></p>
         <!-- <input type="text" #animalDiet> -->
         <div>
           <input name="diet" type="radio" (change)="animalDiet='Carnivore'">Carnivore
@@ -36,19 +36,19 @@ import { Animal } from './animal.model';
         </div>
       </div>
       <div class="body-section">
-        <p><span>Location:</span></p>
+        <p><span>Location</span></p>
         <input type="text" #animalLocation>
         <br>
-        <p><span>Number of Caretakers:</span></p>
+        <p><span>Number of Caretakers</span></p>
         <input type="number" #animalCaretakers>
         <br>
-        <p><span>Likes:</span></p>
+        <p><span>Likes</span></p>
         <input type="text" #animalLikes>
         <br>
-        <p><span>Dislikes:</span></p>
+        <p><span>Dislikes</span></p>
         <input type="text" #animalDislikes>
         <br>
-        <p><span>Image URL (If none available, leave blank):</span></p>
+        <p><span>Image URL (If none available, leave blank)</span></p>
         <!-- <input name="image" type="checkbox" (change)="animalImg=$event.target.checked">-->
         <input type="text" #animalImg>
       </div>
@@ -74,8 +74,10 @@ export class NewAnimalComponent {
 
 
   submitForm(species, name, age, sex, diet, location, caretakers, likes, dislikes, img ){
-    let newAnimalToAdd: Animal = new Animal(species, name, age, sex, diet, location, caretakers, likes, dislikes, img);
-    this.newAnimalSender.emit(newAnimalToAdd);
+    if (species !== '' && name !== '' && age !== '' && sex !== '' && diet !== '' && location !== '' && likes !== '' && dislikes !== ''){
+      let newAnimalToAdd: Animal = new Animal(species, name, age, sex, diet, location, caretakers, likes, dislikes, img);
+      this.newAnimalSender.emit(newAnimalToAdd);
+    }
   }
   getForm(){
     this.newAnimal = true;
